bfa1ca3de7a3cfa70d877bdd6c444fad200e4e23
ordinary120525 <ordinary120525@gmail.com>
2024-11-15 17:11:58 +0900

    15일 2차 수정

--- public/js/loading.js
+++ public/js/loading.js
@@ -1,18 +1,22 @@
-// // 로딩 화면이 3초 후에 사라지도록 설정
-// // setTimeout(() => {
-// //   document.querySelector("#loading-screen").style.display = "none";
-// // }, 3000);
+window.addEventListener("load", function () {
+  const loadingScreen = document.getElementById("loading-screen");
+  const h1Element = document.querySelector(".figure h1");
+  const figureElement = document.querySelector(".figure");
+  const videoElement = document.querySelector(".figure video");
 
-// // 페이지가 로드된 후 3초 후 로딩 화면을 서서히 사라지게 하고 콘텐츠를 표시
-// window.onload = function () {
-//   setTimeout(function () {
-//     // 로딩 화면 서서히 사라지게
-//     document.querySelector("#loading-screen").style.opacity = "0";
+  // 로딩 스크린 3초 후 사라짐
+  setTimeout(function () {
+    loadingScreen.style.display = "none"; // 로딩 스크린 숨기기
+    wrap.style.display = "block"; // 전체 페이지를 표시
+  }, 3000); // 로딩 화면 유지 시간
 
-//     // 로딩 화면이 사라지면 콘텐츠 표시
-//     setTimeout(function () {
-//       document.querySelector("#loading-screen").style.display = "none";
-//       document.querySelector(".wrap").style.display = "block";
-//     }, 3000); // opacity가 0으로 바뀌고 2초 후에 로딩 화면을 숨깁니다
-//   }, 3000); // 3초 후 로딩 화면이 서서히 사라지도록
-// };
+  // 로딩 스크린이 사라진 후 3초 뒤에 애니메이션 시작
+  setTimeout(() => {
+    // 3초 뒤에 h1 애니메이션 시작
+    h1Element.style.opacity = "1"; // h1 태그 보이게 설정
+    h1Element.style.transform = "translate(-50%, -50%)"; // h1 제자리로 이동
+
+    // 비디오 크기가 10% 더 커지며 3초 동안 원래 크기로 줄어듬
+    figureElement.classList.add("active"); // .active 클래스를 추가하여 비디오 크기 애니메이션 시작
+  }, 3000); // 3초 뒤에 h1 애니메이션 시작
+});
